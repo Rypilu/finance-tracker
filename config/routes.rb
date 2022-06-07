7952f054e4bd0d53348b13b8f320d502b6fe9105
rypilu <rpiluski@gmail.com>
2022-06-07 08:07:35 -0400

    stocks controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # welcome = name of controller home = name of method in controller app/controllers/...
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
