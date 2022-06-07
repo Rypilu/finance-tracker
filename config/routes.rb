0b78e5d9950a1d50dfbdce405c37134bf9ded0df
rypilu <rpiluski@gmail.com>
2022-06-07 07:47:58 -0400

    front end stuff for ticker search

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # welcome = name of controller home = name of method in controller app/controllers/...
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
 end
