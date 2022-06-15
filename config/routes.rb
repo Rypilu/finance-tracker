7d82776391491df923a386571135817152863e57
rypilu <rpiluski@gmail.com>
2022-06-15 07:53:10 -0400

    add JS response to form submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
-  # welcome = name of controller home = name of method in controller app/controllers/...
+  # welcome = name of controller index = name of method in controller app/controllers/...
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
 end
