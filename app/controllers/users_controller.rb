49807075a7fa06fe2c68e78d3dc4732fd74a453e
rypilu <rpiluski@gmail.com>
2022-06-16 06:47:30 -0400

    display search results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
 
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result'}
         end
@@ -24,7 +25,7 @@ class UsersController < ApplicationController
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please enter a friend name or email to search"
+        flash.now[:alert] = "Please enter a friends name or email to search"
         format.js { render partial: 'users/friend_result'}
       end
     end
